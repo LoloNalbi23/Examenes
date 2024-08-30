0a195be982e768a9440f2b772e50e530741d8d87
LoloNalbi23 <d48599689@alumnos.itr.edu.ar>
2024-08-30 08:49:50 -0300

    termine el 1

--- ejercicio1examen3.js
+++ ejercicio1examen3.js
@@ -1,14 +1,12 @@
-function isDivisible(a,b) {
+function isDivisible(a,b,numeros) {
     if(a%b===0){
-        numeros.push(a/b)
+        console.log(a/b)
+        console.log(numeros.filter((n)=>n%5===0));
     }else {
         return false;
     }
 }
-
-
 let numeros = [3,78,90,45,69,20];
 let a = 40;
 let b = 20;
-console.log(isDivisible(a,b))
-console.log(numeros.filter((n)=>n%5===0))
+isDivisible(a,b,numeros);
